d9691f7d0bc1985aa209ac16c248e0013d8e1d86
Asim Khan <asimk1308@gmail.com>
2019-09-09 13:35:04 +0530

    zuul,eureka,reocmmendation

---
+++ recommendation-service/src/main/java/com/example/demo/pojo/Movie.java
@@ -0,0 +1,53 @@
+package com.example.demo.pojo;
+
+	public class Movie {
+		private Integer id;
+
+		private String name;
+
+		private String synopsis;
+		
+		
+		
+		
+
+		public Movie() {
+			super();
+			// TODO Auto-generated constructor stub
+		}
+
+		public Movie(Integer id, String name, String synopsis) {
+			super();
+			this.id = id;
+			this.name = name;
+			this.synopsis = synopsis;
+		}
+
+		public Integer getId() {
+			return id;
+		}
+
+		public void setId(Integer id) {
+			this.id = id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getSynopsis() {
+			return synopsis;
+		}
+
+		public void setSynopsis(String synopsis) {
+			this.synopsis = synopsis;
+		}
+
+		// getters and setters
+		
+	}
+
